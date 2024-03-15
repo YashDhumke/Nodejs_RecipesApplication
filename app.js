3302c6a6f68df3b5a4e533f58ed5d110fec9bd38
YashDhumke <89930129+YashDhumke@users.noreply.github.com>
2024-03-15 13:24:29 +0530

    memorystore commit

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
+const MemoryStore = require('memorystore')(session)
+
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: 'keyboard cat'
+}))
 
 require('dotenv').config();
 
